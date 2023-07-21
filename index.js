c0aa5b9d4b5f23b89dce16db997357ce0751eae6
Ashley Frank <ashley.frank@ufl.edu>
2023-07-21 18:19:38 -0400

    updated JS code

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 //.......Start Main Page........
+var form = document.getElementById('form')
 
+console.log(pollenInfo)
+
+form.onsubmit = function(e){
+    var dataEl = document.querySelector('input').value
+    //e.preventDefault()
+    var userInput = document.getElementById("form-textBox").value
+    localStorage.setItem('user_input', userInput)
+    return false
+}
+
+
+var location = localStorage.getItem('user_input')
+pollenInfo.innerHTML = location
 
 
 //.......End Main Page........
@@ -10,7 +24,8 @@
 
 
 //.......Start Pollen Page........
-
+// var result = document.getElementsByClassName('pollen-results')
+// result.innerHTML = localStorage.getItem("userInput")
 
 
 //.......End Pollen Page........
