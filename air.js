8ac83dbab96904572000e939eea2bad20c8d2362
MindfulOlivka <o.campbell@ufl.edu>
2023-07-21 21:11:45 -0400

    adding posible JS functions, search box, changed css

---
+++ air.js
@@ -0,0 +1,38 @@
+const form = document.querySelector(".search-form")
+const locationInput = document.getElementById("air-search")
+const resultsSection = document.querySelector(".allergy-results")
+const locationName = document.getElementById("locationName")
+
+const API_KEY = 
+
+
+ // Function to fetch air quality data based on location
+async function fetchAirQualityData(location) {
+}
+
+
+localStorage.setItem("airQualityData", JSON.stringify(values))
+
+
+ // Function to handle the form submission
+function handleFormSubmit(event) {
+  event.preventDefault()
+  const location = locationInput.value.trim()
+}
+
+
+// Function to load previous search data from local storage
+function loadPreviousSearchData() {
+  const location = localStorage.getItem("location")
+}
+
+
+
+// Load previous search data on page load
+document.addEventListener()
+
+
+// Attach the form submission event listener  ??
+form.addEventListener("submit", handleFormSubmit)  
+
+
