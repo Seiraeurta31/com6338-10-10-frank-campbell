a069dab4bd3fa2ffe7a867822d8c38979ef8e595
Ashley Frank <ashley.frank@ufl.edu>
2023-07-22 21:06:43 -0400

    updated pollen page html and js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//.......Start Main Page........
 
 var form = document.getElementById('form')
 var pollenBtn = document.getElementById("pollenButton").addEventListener("click", processInput)
@@ -10,20 +9,7 @@ async function processInput (e){
     if(!location) return
 
     localStorage.setItem('Location', location)
-    
-    try{
-        //input user location input to recieve location info
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=08ace7633004d5ddf370678a8c052e90`)
-        const locationInfo = await res.json() 
-        console.log(locationInfo) //verify info in console
-
-        //format data for lat long data
-        formatLocationInfo(locationInfo)
-
-    } catch(err){
-        form.locationSearch.value = ""
-    } 
-
+ 
     //validates user button choice
     if(this.id == "pollenButton"){
         //switches to pollen page
@@ -35,38 +21,3 @@ async function processInput (e){
     }
 }
 
-//format and retrieves lat and long from json data
-async function formatLocationInfo(locationInfo){
-    //***enter code HERE to isolate lat and long values from location data */
-    const lat = ""
-    const long = ""
-
-    //store location in local storage
-    localStorage.setItem('LocationLat', lat)
-    localStorage.setItem('LocationLong', long)
-   
-}
-
-
-//.......End Main Page........
-
-
-
-//.......Start Pollen Page........
-
-
-
-
-//.......End Pollen Page........
-
-
-
-
-
-
-//.......Start Air Quality Page........
-
-
-
-
-//.......End Air Quality Page........
