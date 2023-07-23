00740a5f9852faa880b66855350f6a79406c4263
MindfulOlivka <o.campbell@ufl.edu>
2023-07-23 18:33:23 -0400

    AQ html and js changes, added data results

--- air.js
+++ air.js
@@ -1,9 +1,13 @@
-const apiKey = 'c2d66421775a47b39aa2a5e2b0de6bbe'
+//const apiKey = 'c2d66421775a47b39aa2a5e2b0de6bbe'
 var form = document.getElementById('search-form')
-var airResults = document.getElementById('air-results')
-var airHeadline = document.getElementById('airHeader1')
+var ResultsOfAQ = document.getElementById('air-results')
+var airTop = document.getElementById('airTop')
 var storedLocation = localStorage.getItem('Location')
-var airResultsDiv = document.getElementById("air-results_div");
+
+airTop.innerHTML = `Air Quality`
+ResultsOfAQ.innerHTML =" "
+
+var searchBtn = document.getElementById("searchBtn").addEventListener("click", newLocation)
 
 if (storedLocation) {
   storedLocation =
@@ -11,46 +15,56 @@ if (storedLocation) {
   localStorage.setItem("Location", storedLocation)
 }
 
-airResults.innerHTML = storedLocation
-airHeadline.innerHTML = `Air Quality for ${storedLocation}`
 
-var searchBtn = document.getElementById("searchBtn").addEventListener("click", newLocation)
+longLat(storedLocation)
 
-async function latLongData (storedLocation){
-    try {
-    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${storedLocation}&units=imperial&appid=c2d66421775a47b39aa2a5e2b0de6bbe`)
-    if(res.status !== 200) throw new Error ('Location not found')
-    const locationInfo = await res.json()
-    console.log(locationInfo)
+async function longLat (storedLocation){
+    try{
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${storedLocation},US&units=imperial&appid=08ace7633004d5ddf370678a8c052e90`
+    )
+    if(res.status !== 200) throw new Error('Location not found')
+    airTop.innerHTML = `Air Quality in ${storedLocation}`
+    var locationInfo = await res.json()
+
+     const {
+       coord: { 
+        lat,
+        lon 
+    }
+     } = locationInfo
+
+     getAirInfo(lat, lon)
 
   } catch (err) {
-    airResults.innerHTML = err.message
+    airTop.innerHTML = err.message
+    return
   }
-
-
-
-const displayData = (data) => {
-
-    const location2 = document.createElement('h2')
-    const {
-        name, 
-        country,
-        baqi: {aqi_display, color, category, dominant_pollutant}
-    } = data
-    location2.textContent = `${name}, ${country}`
-    airResultsDiv.appendChild(location2)
-
 }
 
-const createAndAppend = (tagName, text) => {
-    const element = document.createElement(tagName)
-    element.textContent = text
-    airResultsDiv.appendChild(element)
-  }
-
-  createAndAppend("p", `AQI: ${aqi_display}`)
-  createAndAppend("p", `Level: ${color}`)
-  createAndAppend("p", `Quality: ${category}`)
-  createAndAppend("p", `Dominant Pollutant: ${dominant_pollutant}`)
+async function newLocation(){
+  var location = document.querySelector('input').value.trim()
+  
+  localStorage.setItem('Location', location)
+ 
 }
 
+async function getAirInfo(lat, lon){
+  try{
+    const res = await fetch(
+      `https://api.breezometer.com/air-quality/v2/current-conditions?lat=${lat}&lon=${lon}&key=c2d66421775a47b39aa2a5e2b0de6bbe&features=breezometer_aqi,local_aqi,health_recommendations,sources_and_effects,pollutants_concentrations,pollutants_aqi_information`
+    )
+    if (res.status !== 200) throw new Error('Information not found')
+    var dataAq = await res.json()
+    
+
+  ResultsOfAQ.innerHTML = 
+  `<p>${`Air Quality Index: ${dataAq.data.indexes.baqi.aqi_display}`}</p>
+    <p>${`Level: ${dataAq.data.indexes.baqi.category}`}</p>
+     <p>${`Dominant Pollutant: ${dataAq.data.indexes.baqi.dominant_pollutant}`}</p>`
+
+  }
+catch(err){
+    ResultsOfAQ.innerHTML = err.message
+  }
+}
