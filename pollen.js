ea15a16332d0b4e0ee804e6ffbec6104cae8930e
Ashley Frank <ashley.frank@ufl.edu>
2023-07-24 19:20:12 -0400

    updated JS files to use same lat/long() in index

--- pollen.js
+++ pollen.js
@@ -3,68 +3,68 @@ var form = document.getElementById('search-form')
 var pollenResults = document.getElementById('pollen-results')
 var pollenHeadline = document.getElementById('pollenHeader1')
 var storedLocation = localStorage.getItem('Location')
+var searchPlaceholder = document.getElementById('allergySearch')
+var lat = localStorage.getItem('LocationLat')
+var lon = localStorage.getItem('LocationLon')
 
-//pollenResults.innerHTML = storedLocation
-pollenHeadline.innerHTML = `Local Pollen Levels`
-pollenResults.innerHTML =" "
 
-var searchBtn = document.getElementById("searchBtn").addEventListener("click", newLocation)
+//set new search buton and confirm if button id exists before adding event listner
+var searchBtn = document.getElementById("searchBtn")
+if(searchBtn){
+    searchBtn.addEventListener("click", newLocation)
+}
 
-if (storedLocation) {
-    storedLocation =
-      storedLocation.charAt(0).toUpperCase() + storedLocation.slice(1).toLowerCase()
-    localStorage.setItem("Location", storedLocation)
-  }
-
-longLat(storedLocation)
-
-//Retrieves lat and long data from map API
-async function longLat (storedLocation){
-    console.log("function triggered")
-    try{
-        //input user location input to recieve location info
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${storedLocation},US&units=imperial&appid=08ace7633004d5ddf370678a8c052e90`)
-        if(res.status !== 200) throw new Error('Location not found')
-        pollenHeadline.innerHTML = `Pollen Levels in ${storedLocation}`
-        var locationInfo = await res.json() 
-        //console.log(locationInfo) //verify info in console
+//Format user input to proper capitalization
+if(storedLocation){
+    storedLocation = storedLocation.charAt(0).toUpperCase() + storedLocation.slice(1).toLowerCase()
+    localStorage.setItem('Location', storedLocation)
+    console.log ("if location exists triggered")
     
-        const {
-            coord: {
-                lat,
-                lon
-            }
-        } = locationInfo
-
-        getPollenInfo(lat,lon)  
-    
-    } catch(err){
-        pollenResults.innerHTML =" "
-        pollenHeadline.innerHTML = err.message
-        return
-    } 
 }
 
 
-//Sets new location into storage and calls on function to get lat lon from new location
-async function newLocation(){
-    var location = document.querySelector('input').value.trim() 
-    localStorage.setItem('Location', location)
-    longLat(storedLocation)
+//Call function to get Pollen info with stored lat/long data
+getPollenInfo(lat, lon)
+
+//Sets new search location into storage and calls on function to get lat lon from new location
+async function newLocation(e){
+    e.preventDefault() 
+    console.log("new location triggered")
+    
+    //Take in user input with new search, format it, store it, then convert to new lat/long
+    var newLocation = document.querySelector('input').value.trim() 
+    newLocation = newLocation.charAt(0).toUpperCase() + newLocation.slice(1).toLowerCase()
+    localStorage.setItem('Location', newLocation)
+    storedLocation = localStorage.getItem('Location')
+    // storedLocation = storedLocation.charAt(0).toUpperCase() + storedLocation.slice(1).toLowerCase()
+    
+    await longLat(storedLocation)
+
+    //Access new lat/long data saved to localStorage, then access pollen info for location
+    lat = localStorage.getItem('LocationLat')
+    lon = localStorage.getItem('LocationLon')
+    
+    getPollenInfo(lat, lon)
 }
 
 
 async function getPollenInfo(lat, lon){
-    //use longitude latitude values to access pollen API information
+
+    //Empty results to prepare for new info, update headline with new location
+    // pollenResults.innerHTML =""
+    searchPlaceholder.value = ""
+    pollenHeadline.innerHTML = `Local Pollen Levels for ${storedLocation}`
+    
+    //Use longitude latitude values to access pollen API information
     try{
-        const res = await fetch(`https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lon}&days=1&key=d8f9dcc96f87425a8e375f4f7ae2c788&features=types_information,plants_information`)
+        const res = await fetch(`https://api.breezometer.com/pollen/v2/forecast/daily?lat=${lat}&lon=${lon}&days=1&key=c2d66421775a47b39aa2a5e2b0de6bbe&features=types_information,plants_information`)
         if(res.status !== 200) throw new Error('Information not found')
+        
         var pollenData = await res.json() 
-        console.log(pollenData) //verify info in console
     } catch(err){
         pollenResults.innerHTML = err.message
     } 
-
+    
     var grassValue = "Not in season"
     var weedValue = "Not in season"
     var treeValue = "Not in season"
